393ef7c3b11df5b499ebd4d4b68be625c5797e99
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-07-19 22:25:21 +0200

    Initial gatsby commit

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "thomasemilsson.info",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
+};
