7c2992171d38bc8779550d243e613cc15667c368
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-07-20 03:34:48 +0200

    Add PaletteTester Component to test color schemes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react";
 import "../scss/main.scss";
+import Tester from "../components/palette-tester/paletteTester";
 
 // markup
 const IndexPage = () => {
   return (
     <main>
       <title>🇸🇪 🇵🇪</title>
+      <Tester />
     </main>
   );
 };
