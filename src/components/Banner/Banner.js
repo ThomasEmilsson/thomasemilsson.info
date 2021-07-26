9de9dcb985f4406a94f5e50256bf60b38ea10ebd
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-07-26 16:19:41 +0200

    Add react-icons; Update Banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,12 @@
 import React from "react";
-
 import "./Banner.scss";
+import {
+  FaLinkedin,
+  FaGithub,
+  FaInstagram,
+  FaFacebookSquare,
+  FaSoundcloud,
+} from "react-icons/fa";
 
 const Banner = () => {
   return (
@@ -8,6 +14,23 @@ const Banner = () => {
       <h1 className="banner-title"> Welcome</h1>
       <p className="banner-description">
         I am a Software Engineer, Web Developer, Musician, and an Avid Climber
+        <p className="banner-icons">
+          <a href="">
+            <FaLinkedin />
+          </a>
+          <a href="">
+            <FaGithub />
+          </a>
+          <a href="">
+            <FaSoundcloud />
+          </a>
+          <a href="">
+            <FaInstagram />
+          </a>
+          <a href="">
+            <FaFacebookSquare />
+          </a>
+        </p>
       </p>
     </div>
   );
