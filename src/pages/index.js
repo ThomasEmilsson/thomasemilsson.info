1a63c84665a0167083a992ac53b2f980e8d01ad6
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-08-01 13:19:36 +0200

    Add Footer; Complete basic styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,23 @@
 import * as React from "react";
 import "../scss/main.scss";
 import Header from "../components/Header/Header";
-// import Card from "../components/Card/Card";
 import OptionList from "../components/OptionList/OptionList";
 import Banner from "../components/Banner/Banner";
+import Footer from "../components/Footer/Footer";
 
 const IndexPage = () => {
   return (
     <main>
-      <title>🇸🇪 🇵🇪</title>
-      <Header />
-      <div className="diagonal-background">
-        {/* <div style={{ paddingTop: "20px" }} /> */}
-        <Banner />
-      </div>
-      <OptionList />
+      <content>
+        <title>🇸🇪 🇵🇪</title>
+        <Header />
+        <div className="diagonal-background">
+          <Banner />
+        </div>
+        <OptionList />
+      </content>
+      <div className="diagonal-background-inverse" />
+      <Footer />
     </main>
   );
 };
