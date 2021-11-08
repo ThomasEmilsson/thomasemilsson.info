b8a156558a255451a3300d2b54bc4c755c95a38d
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-11-08 05:49:07 +0100

    Fix grid for experience-date and hero-details

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -8,20 +8,12 @@ import Experiences from "./Experiences";
 const Resume = () => {
   let content = CVObject;
 
-  let latin =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur";
-  let latinShort =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam";
-
-  // content.Experiences.forEach((data) => console.log(data));
-  // console.log(content.Experience);
-
   return (
     <div className="resume-div">
       <div className="resume-separator"></div>
       <div className="resume-introduction-div">
         <div className="resume-title">Resume</div>
-        <div className="resume-description">{latin}</div>
+        <div className="resume-description">{}</div>
       </div>
 
       <Experiences content={content.Experiences} />
