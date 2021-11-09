950584b92edf7bcdc85d99c71a07c31eb4c725d3
Thomas Emilsson <thomasemilsson10@gmail.com>
2021-11-09 08:06:50 +0100

    Minor css updates for ReadOnlyCheckBox

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,21 +5,30 @@ import "./About.scss";
 const About = () => {
   return (
     <div>
-      <ReadOnlyCheckBox title="test" status={true} />
+      <ReadOnlyCheckBox title="test" status={false} indent={1} />
+      <ReadOnlyCheckBox title="test" status={true} indent={2} />
+      <ReadOnlyCheckBox title="test" status={false} indent={3} />
     </div>
   );
 };
 
-const ReadOnlyCheckBox = ({ title, status }) => {
+const ReadOnlyCheckBox = ({ title, status, indent }) => {
   const doNothing = (event) => {
     event.target.checked = !event.target.checked;
   };
 
   return (
-    <label>
-      <input type="checkbox" onChange={doNothing} checked={status} />
-      <span>{title}</span>
-    </label>
+    <div className={`indent-${indent}`}>
+      <label>
+        <input
+          type="checkbox"
+          // className={`indent-${indent}`}
+          onChange={doNothing}
+          checked={status}
+        />
+        <span>{title}</span>
+      </label>
+    </div>
   );
 };
 
